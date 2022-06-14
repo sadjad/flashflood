668e9351f5dc6323ae8d6f6036205aad17db1906
Sadjad Fouladi <sfouladi@gmail.com>
2022-06-14 14:50:25 -0700

    Bring the latest stuff from R2E2.

---
+++ src/util/tokenize.hh
@@ -0,0 +1,9 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+std::vector<std::string> split( const std::string& str,
+                                const std::string& separator );
